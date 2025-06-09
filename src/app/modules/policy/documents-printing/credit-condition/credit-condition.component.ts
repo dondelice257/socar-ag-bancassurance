96724c5c0c8797c5c91a439c120a666129478837
dondelice257 <dondelicedushime@gmail.com>
2025-06-09 12:39:30 +0100

    add qr code on credit-condition

--- src/app/modules/policy/documents-printing/credit-condition/credit-condition.component.ts
+++ src/app/modules/policy/documents-printing/credit-condition/credit-condition.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-credit-condition',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    QRCodeModule,
+
   ],
   templateUrl: './credit-condition.component.html',
   styleUrl: './credit-condition.component.scss'
